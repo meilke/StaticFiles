f219f196bb9e855a23abe498ced2c84ef70cd3a5
meilke <christian.meilke@googlemail.com>
2013-08-15 12:58:16 +0200

    Initial commit

---
+++ StaticFiles.Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StaticFiles.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(WebApp.Start<Startup>("http://localhost:8081"))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+
+            var staticFileDirectoryPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "web");
+            app.UseStaticFiles(staticFileDirectoryPath);
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            app.UseWebApi(config);
+        }
+    }
+}
